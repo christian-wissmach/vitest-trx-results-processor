bc5099c0c75a592ec234b99cf8286045ffd92bda
(External) Christian Wißmach <christian.wissmach@diamant-software.de>
2025-02-04 11:53:56 +0100

    feat(module): vitest version of the trx results processor
    If you use vitest instead of jest you can use this trx processor to create your trx reports
    
    BREAKING CHANGE: You need to change your testing framework from jest to vitest

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-// Magic GUID's and other constants
+// Magic GUID"s and other constants
 export const testListNotInListId = "8c84fa94-04c1-424b-9868-57a2d4851a1d";
 export const testListAllLoadedResultsId = "19431567-8539-422a-85d7-44ee4e166bda";
 export const testType = "13cdc9d9-ddb5-4fa4-a97d-d965ccfc6d4b";
@@ -7,7 +7,7 @@ export const testOutcomeTable: { [outcome: string]: string } = {
   failed: "Failed",
   passed: "Passed",
   pending: "NotExecuted",
-  todo: "NotExecuted",
+  skipped: "NotExecuted",
 };
 
 export const defaultOutputFile = "test-results.trx";
