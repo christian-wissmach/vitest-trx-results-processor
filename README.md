2a3353612def30dbd62ab341ef35842b322bb08d
(External) Christian Wißmach <christian.wissmach@diamant-software.de>
2025-02-04 11:53:56 +0100

    feat(module): vitest version of the trx results processor
    If you use vitest instead of jest you can use this trx processor to create your trx reports
    
    BREAKING CHANGE: You need to change your testing framework from jest to vitest

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-![Node.js CI](https://github.com/no23reason/jest-trx-results-processor/workflows/Node.js%20CI/badge.svg)
-[![npm version](https://img.shields.io/npm/v/jest-trx-results-processor.svg)](https://www.npmjs.com/package/jest-trx-results-processor)
-[![Dependency Status](https://david-dm.org/no23reason/jest-trx-results-processor.svg)](https://david-dm.org/no23reason/jest-trx-results-processor)
+![Node.js CI](https://github.com/christian-wissmach/vitest-trx-results-processor/workflows/Node.js%20CI/badge.svg)
+[![npm version](https://img.shields.io/npm/v/vitest-trx-results-processor.svg)](https://www.npmjs.com/package/vitest-trx-results-processor)
+[![Dependency Status](https://david-dm.org/christian-wissmach/vitest-trx-results-processor.svg)](https://david-dm.org/christian-wissmach/vitest-trx-results-processor)
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 
-# jest-trx-results-processor
+# vitest-trx-results-processor
 
-This package is used to export a TRX file from [Jest](https://facebook.github.io/jest/) test runs to be used in [Visual Studio](https://www.visualstudio.com/) and [Visual Studio Team Services](https://www.visualstudio.com/vsts-test/).
+This package is used to export a TRX file from [Vitest](https://vitest.dev/) test runs to be used in [Visual Studio](https://www.visualstudio.com/) and [Visual Studio Team Services](https://www.visualstudio.com/vsts-test/).
 
 ## Migration from 0.x
 
@@ -15,7 +15,7 @@ Version 1.0.0 was rewritten to use reporters API. Either use that (see [Usage](#
 ## Installation
 
 ```
-yarn add --dev jest-trx-results-processor
+yarn add --dev vitest-trx-results-processor
 ```
 
 ## Usage
@@ -24,7 +24,7 @@ In your jest config add the following entry:
 
 ```json
 {
-  "reporters": ["default", "jest-trx-results-processor"]
+  "reporters": ["default", "vitest-trx-results-processor"]
 }
 ```
 
@@ -35,7 +35,7 @@ You can also pass additional arguments:
   "reporters": [
     "default",
     [
-      "jest-trx-results-processor",
+      "vitest-trx-results-processor",
       {
         "outputFile": "relative/path/to/resulting.trx", // defaults to "test-results.trx"
         "defaultUserName": "user name to use if automatic detection fails" // defaults to "anonymous"
@@ -45,20 +45,20 @@ You can also pass additional arguments:
 }
 ```
 
-Then run jest as usual.
+Then run vitest as usual.
 
 ## Usage as testResultsProcessor
 
 _Notice: this method of use will be removed in the next major version. Please use the reporters API described above._
 
-Create a `jestTrxProcessor.js` file somewhere in your project (for this example I'll assume the `scripts` folder).
+Create a `vitestTrxProcessor.js` file somewhere in your project (for this example I'll assume the `scripts` folder).
 There you can configure the processor, as Jest does not allow you to pass custom parameters to the results processor:
 
 ```js
-// for jest-trx-results-processor >= 1.0.0
-var builder = require("jest-trx-results-processor/dist/testResultsProcessor"); // only this has changed since v 0.x
-// for jest-trx-results-processor < 1.0.0
-var builder = require("jest-trx-results-processor");
+// for vitest-trx-results-processor >= 1.0.0
+var builder = require("vitest-trx-results-processor/dist/testResultsProcessor"); // only this has changed since v 0.x
+// for vitest-trx-results-processor < 1.0.0
+var builder = require("vitest-trx-results-processor");
 
 var processor = builder({
   outputFile: "relative/path/to/resulting.trx", // this defaults to "test-results.trx"
@@ -68,28 +68,29 @@ var processor = builder({
 module.exports = processor;
 ```
 
-Finally, point Jest to your results processor in the `package.json`:
+Finally, point Vitest to your results processor in the `package.json`:
 
 ```json
 {
   "devDependencies": {
-    "jest": "^19.0.0",
-    "jest-trx-results-processor": "~0.0.5"
+    "vitest": "^3.0.5",
+    "vitest-trx-results-processor": "~0.0.5"
   },
   "jest": {
-    "testResultsProcessor": "./scripts/jestTrxProcessor"
+    "testResultsProcessor": "./scripts/vitestTrxProcessor"
   }
 }
 ```
 
-Then you just run Jest as you normally would.
+Then you just run Vitest as you normally would.
 
-Minimal working configuration can be seen in the [examples folder](https://github.com/no23reason/jest-trx-results-processor/tree/master/examples).
+Minimal working configuration can be seen in the [examples folder](https://github.com/christian-wissmach/vitest-trx-results-processor/tree/master/examples).
 
 ## Acknowledgements
 
-This tool is heavily inspired by [karma-trx-reporter](https://github.com/hatchteam/karma-trx-reporter) and [jest-junit](https://github.com/jest-community/jest-junit).
+This tool is a fork from [jest-trx-results-reporter](https://github.com/no23reason/jest-trx-results-processor) which is
+heavily inspired by [karma-trx-reporter](https://github.com/hatchteam/karma-trx-reporter) and [jest-junit](https://github.com/jest-community/jest-junit).
 
 ## License
 
-_jest-trx-results-processor_ is available under MIT. See [LICENSE](https://github.com/no23reason/jest-trx-results-processor/tree/master/LICENSE) for more details.
+_vitest-trx-results-processor_ is available under MIT. See [LICENSE](https://github.com/christian-wissmach/vitest-trx-results-processor/tree/master/LICENSE) for more details.
--- src/trx-generator.ts
+++ src/trx-generator.ts
@@ -1,24 +1,11 @@
-import {
-  AggregatedResult,
-  AssertionResult,
-  TestResult,
-} from "@jest/test-result";
-import * as path from "path";
-import { v4 as uuidv4 } from "uuid";
-import { create as createXmlBuilder, XMLElement } from "xmlbuilder";
+import * as path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import { XMLElement, create as createXmlBuilder } from 'xmlbuilder';
 
-import {
-  testListAllLoadedResultsId,
-  testListNotInListId,
-  testOutcomeTable,
-  testType,
-} from "./constants";
-import {
-  formatDuration,
-  getEnvInfo,
-  getFullTestName,
-  getTestClassName,
-} from "./utils";
+import { SerializedError } from '@vitest/utils';
+import { TestCase, TestModule, TestSuite } from 'vitest/node';
+import { testListAllLoadedResultsId, testListNotInListId, testOutcomeTable, testType } from './constants';
+import { formatDuration, getEnvInfo } from './utils';
 
 /**
  * All the configuration options.
@@ -40,274 +27,240 @@ export interface IOptions {
    * Set of methods that may be used to augment the resulting trx file.
    * Each of these methods are called after the testResultNode has been generated.
    */
-  postProcessTestResult?: [
-    (
-      testSuiteResult: TestResult,
-      testResult: AssertionResult,
-      testResultNode: XMLElement,
-    ) => void,
-  ];
+  postProcessTestResult?: [(testSuiteResult: TestSuite, testResult: TestCase, testResultNode: XMLElement) => void];
 }
 
-const renderTestRun = (
-  builder: XMLElement,
-  testRunResult: AggregatedResult,
-  computerName: string,
-  userName: string,
-): void => {
+const renderTestRun = (builder: XMLElement, testModule: TestModule, computerName: string, userName: string): void => {
+  const startTime = testModule.children.tests().next().value?.diagnostic()?.startTime ?? 0;
   builder
-    .att("id", uuidv4())
-    .att(
-      "name",
-      `${userName}@${computerName} ${new Date(
-        testRunResult.startTime,
-      ).toISOString()}`,
-    )
-    .att("runUser", userName)
-    .att("xmlns", "http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+      .att('id', uuidv4())
+      .att('name', `${userName}@${computerName} ${new Date(startTime).toISOString()}`)
+      .att('runUser', userName)
+      .att('xmlns', 'http://microsoft.com/schemas/VisualStudio/TeamTest/2010');
 };
 
 const renderTestSettings = (parentNode: XMLElement): void => {
-  parentNode
-    .ele("TestSettings")
-    .att("name", "Jest test run")
-    .att("id", uuidv4());
+  parentNode.ele('TestSettings').att('name', 'Jest test run').att('id', uuidv4());
 };
 
-const renderTimes = (
-  parentNode: XMLElement,
-  testRunResult: AggregatedResult,
-): void => {
-  const startTime = new Date(testRunResult.startTime).toISOString();
-  const totalDuration = testRunResult.testResults.reduce((acc, tr) => acc + tr.perfStats.runtime, 0);
-  const finishTime = new Date(testRunResult.startTime + totalDuration).toISOString();
+const renderTimes = (parentNode: XMLElement, testModules: TestModule[]): void => {
+  const startTime = testModules[0].children.tests().next().value?.diagnostic()?.startTime ?? 0;
+  let totalDuration = 0;
+  for (const testModule of testModules) {
+    totalDuration = totalDuration + testModule.diagnostic().duration;
+  }
+  const startDate = new Date(startTime).toISOString();
+  const finishDate = new Date(startTime + totalDuration).toISOString();
 
   parentNode
-    .ele("Times")
-    .att("creation", startTime)
-    .att("queuing", startTime)
-    .att("start", startTime)
-    .att("finish", finishTime);
+      .ele('Times')
+      .att('creation', startDate)
+      .att('queuing', startDate)
+      .att('start', startDate)
+      .att('finish', finishDate);
 };
 
-const renderResultSummary = (
-  parentNode: XMLElement,
-  testRunResult: AggregatedResult,
-): void => {
-  // workaround for https://github.com/facebook/jest/issues/6924
-  const anyTestFailures = !(
-    testRunResult.numFailedTests === 0 &&
-    testRunResult.numRuntimeErrorTestSuites === 0
-  );
+const renderResultSummary = (parentNode: XMLElement, testModules: TestModule[]): void => {
+  const failedTests = [];
+  const passedTests = [];
+  const pendingTests = [];
+  const allTests = [];
 
-  const isSuccess = !(anyTestFailures || testRunResult.snapshot.failure);
+  let errors: SerializedError[] = [];
 
-  const summary = parentNode
-    .ele("ResultSummary")
-    .att("outcome", isSuccess ? "Passed" : "Failed");
+  for (const testModule of testModules) {
+    const tests = testModule.children.allTests();
+    if (testModule.errors() && testModule.errors().length > 0) {
+      errors = errors.concat(testModule.errors());
+    }
+
+    for (const test of tests) {
+      if (test.result().state === 'failed') {
+        failedTests.push(test);
+      } else if (test.result().state === 'passed') {
+        passedTests.push(test);
+      } else if (test.result().state === 'pending' || test.result().state === 'skipped') {
+        pendingTests.push(test);
+      }
+      allTests.push(test);
+    }
+  }
+  
+
+  const anyTestFailures = failedTests.length > 0;
+
+  const isSuccess = !(anyTestFailures || errors.length > 0);
+
+  const summary = parentNode.ele('ResultSummary').att('outcome', isSuccess ? 'Passed' : 'Failed');
 
   summary
-    .ele("Counters")
-    .att(
-      "total",
-      testRunResult.numTotalTests + testRunResult.numRuntimeErrorTestSuites,
-    )
-    .att(
-      "executed",
-      testRunResult.numTotalTests - testRunResult.numPendingTests,
-    )
-    .att("passed", testRunResult.numPassedTests)
-    .att("failed", testRunResult.numFailedTests)
-    .att("error", testRunResult.numRuntimeErrorTestSuites);
+      .ele('Counters')
+      .att('total', allTests.length + errors.length)
+      .att('executed', allTests.length - pendingTests.length)
+      .att('passed', passedTests.length)
+      .att('failed', failedTests.length)
+      .att('error', errors.length);
 };
 
 const renderTestLists = (parentNode: XMLElement): void => {
-  const testLists = parentNode.ele("TestLists");
+  const testLists = parentNode.ele('TestLists');
 
-  testLists
-    .ele("TestList")
-    .att("name", "Results Not in a List")
-    .att("id", testListNotInListId);
+  testLists.ele('TestList').att('name', 'Results Not in a List').att('id', testListNotInListId);
 
-  testLists
-    .ele("TestList")
-    .att("name", "All Loaded Results")
-    .att("id", testListAllLoadedResultsId);
+  testLists.ele('TestList').att('name', 'All Loaded Results').att('id', testListAllLoadedResultsId);
 };
 
 const renderTestSuiteResult = (
-  testSuiteResult: TestResult,
-  testDefinitionsNode: XMLElement,
-  testEntriesNode: XMLElement,
-  resultsNode: XMLElement,
-  computerName: string,
-  postProcessTestResult?: [
-    (
-      // eslint-disable-next-line no-shadow
-      testSuiteResult: TestResult,
-      testResult: AssertionResult,
-      testResultNode: XMLElement,
-    ) => void,
-  ],
+    testSuiteResult: TestSuite,
+    testDefinitionsNode: XMLElement,
+    testEntriesNode: XMLElement,
+    resultsNode: XMLElement,
+    computerName: string,
+    postProcessTestResult?: [
+      (
+          // eslint-disable-next-line no-shadow
+          testSuiteResult: TestSuite,
+          testResult: TestCase,
+          testResultNode: XMLElement,
+      ) => void,
+    ],
 ): void => {
-  if (testSuiteResult.testResults && testSuiteResult.testResults.length) {
-    let runningDuration = 0;
-    testSuiteResult.testResults.forEach((testResult) => {
-      const testId = uuidv4();
-      const executionId = uuidv4();
-      const fullTestName = getFullTestName(testResult);
-      const filepath = path.relative("./", testSuiteResult.testFilePath);
-      const duration = testResult.duration || 0;
+  let runningDuration = 0;
+  for (const testResult of testSuiteResult.children.tests()) {
+    const testId = uuidv4();
+    const executionId = uuidv4();
+    const fullTestName = testResult.fullName;
+    const filepath = path.relative('./', testResult.module.moduleId);
+    const duration = testResult.diagnostic()?.duration || 0;
 
-      // UnitTest
-      const unitTest = testDefinitionsNode
-        .ele("UnitTest")
-        .att("name", fullTestName)
-        .att("id", testId)
-        .att("storage", filepath);
-      unitTest.ele("Execution").att("id", executionId);
-      unitTest
-        .ele("TestMethod")
-        .att("codeBase", filepath)
-        .att("name", fullTestName)
-        .att("className", getTestClassName(testResult));
+    // UnitTest
+    const unitTest = testDefinitionsNode
+        .ele('UnitTest')
+        .att('name', fullTestName)
+        .att('id', testId)
+        .att('storage', filepath);
+    unitTest.ele('Execution').att('id', executionId);
+    unitTest
+        .ele('TestMethod')
+        .att('codeBase', filepath)
+        .att('name', fullTestName)
+        .att('className', testSuiteResult.fullName);
 
-      // TestEntry
-      testEntriesNode
-        .ele("TestEntry")
-        .att("testId", testId)
-        .att("executionId", executionId)
-        .att("testListId", testListNotInListId);
+    // TestEntry
+    testEntriesNode
+        .ele('TestEntry')
+        .att('testId', testId)
+        .att('executionId', executionId)
+        .att('testListId', testListNotInListId);
 
-      // UnitTestResult
-      const result = resultsNode
-        .ele("UnitTestResult")
-        .att("testId", testId)
-        .att("executionId", executionId)
-        .att("testName", fullTestName)
-        .att("computerName", computerName)
-        .att("duration", formatDuration(duration))
-        .att(
-          "startTime",
-          new Date(
-            testSuiteResult.perfStats.start,
-          ).toISOString(),
-        )
-        .att(
-          "endTime",
-          new Date(
-            testSuiteResult.perfStats.start + runningDuration,
-          ).toISOString(),
-        )
-        .att("testType", testType)
-        .att("outcome", testOutcomeTable[testResult.status])
-        .att("testListId", testListNotInListId);
+    const startTime = testSuiteResult.children.tests().next().value?.diagnostic()?.startTime ?? 0;
+    // UnitTestResult
+    const result = resultsNode
+        .ele('UnitTestResult')
+        .att('testId', testId)
+        .att('executionId', executionId)
+        .att('testName', fullTestName)
+        .att('computerName', computerName)
+        .att('duration', formatDuration(duration))
+        .att('startTime', new Date(startTime).toISOString())
+        .att('endTime', new Date(startTime + runningDuration).toISOString())
+        .att('testType', testType)
+        .att('outcome', testOutcomeTable[testResult.result().state])
+        .att('testListId', testListNotInListId);
 
-      runningDuration += duration;
+    runningDuration += duration;
 
-      if (testResult.status === "failed") {
-        result
-          .ele("Output")
-          .ele("ErrorInfo")
-          .ele("Message", testResult.failureMessages.join("\n"));
-      }
+    if (testResult.result().state === 'failed') {
+      result.ele('Output').ele('ErrorInfo').ele('Message', testResult.result().errors?.map(e => e.message).join('\n'));
+    }
 
-      // Perform any post processing for this test result.
-      if (postProcessTestResult) {
-        postProcessTestResult.forEach(postProcess =>
-          postProcess(testSuiteResult, testResult, result),
-        );
-      }
-    });
+    // Perform any post processing for this test result.
+    if (postProcessTestResult) {
+      postProcessTestResult.forEach(postProcess => postProcess(testSuiteResult, testResult, result));
+    }
   }
 
-  if (testSuiteResult.testExecError?.stack) {
+  if (testSuiteResult.errors()?.length > 0) {
     // For suites that failed to run, we will generate a test result that documents the failure.
     // This occurs when there is a failure compiling/loading the suite or an assertion in a before/after hook fails,
     // not when a test in the suite fails.
     const testId = uuidv4();
     const executionId = uuidv4();
-    const fullTestName = path.basename(testSuiteResult.testFilePath);
+    const fullTestName = testSuiteResult.fullName;
     const time = new Date().toISOString();
-    const filepath = path.relative("./", testSuiteResult.testFilePath);
+    const filepath = path.relative('./', testSuiteResult.module.moduleId);
 
     // Failed TestSuite
     const unitTest = testDefinitionsNode
-      .ele("UnitTest")
-      .att("name", fullTestName)
-      .att("id", testId)
-      .att("storage", filepath);
-    unitTest.ele("Execution").att("id", executionId);
+        .ele('UnitTest')
+        .att('name', fullTestName)
+        .att('id', testId)
+        .att('storage', filepath);
+    unitTest.ele('Execution').att('id', executionId);
     unitTest
-      .ele("TestMethod")
-      .att("codeBase", filepath)
-      .att("name", fullTestName)
-      .att("className", fullTestName);
+        .ele('TestMethod')
+        .att('codeBase', filepath)
+        .att('name', fullTestName)
+        .att('className', testSuiteResult.fullName);
     // TestEntry
     testEntriesNode
-      .ele("TestEntry")
-      .att("testId", testId)
-      .att("executionId", executionId)
-      .att("testListId", testListNotInListId);
+        .ele('TestEntry')
+        .att('testId', testId)
+        .att('executionId', executionId)
+        .att('testListId', testListNotInListId);
     // UnitTestResult
     const result = resultsNode
-      .ele("UnitTestResult")
-      .att("testId", testId)
-      .att("executionId", executionId)
-      .att("testName", fullTestName)
-      .att("computerName", computerName)
-      .att("duration", "0")
-      .att("startTime", time)
-      .att("endTime", time)
-      .att("testType", testType)
-      .att("outcome", testOutcomeTable.failed)
-      .att("testListId", testListNotInListId);
-    result
-      .ele("Output")
-      .ele("ErrorInfo")
-      .ele("Message", testSuiteResult.testExecError.stack);
+        .ele('UnitTestResult')
+        .att('testId', testId)
+        .att('executionId', executionId)
+        .att('testName', fullTestName)
+        .att('computerName', computerName)
+        .att('duration', '0')
+        .att('startTime', time)
+        .att('endTime', time)
+        .att('testType', testType)
+        .att('outcome', testOutcomeTable['failed'])
+        .att('testListId', testListNotInListId);
+    result.ele('Output').ele('ErrorInfo').ele('Message', testSuiteResult.errors().map(e => e.message).join('\n'));
   }
 };
 
-export const generateTrx = (
-  testRunResult: AggregatedResult,
-  options?: IOptions,
-): string => {
-  const { computerName, userName } = getEnvInfo(
-    options && options.defaultUserName,
-  );
+export const generateTrx = (testModules: TestModule[], options?: IOptions): string => {
+  const { computerName, userName } = getEnvInfo(options && options.defaultUserName);
 
-  const resultBuilder = createXmlBuilder("TestRun", {
-    invalidCharReplacement: "",
-    version: "1.0",
-    encoding: "UTF-8",
+  const resultBuilder = createXmlBuilder('TestRun', {
+    invalidCharReplacement: '',
+    version: '1.0',
+    encoding: 'UTF-8',
   });
 
-  renderTestRun(resultBuilder, testRunResult, computerName, userName);
+  renderTestRun(resultBuilder, testModules[0], computerName, userName);
 
   renderTestSettings(resultBuilder);
 
-  renderTimes(resultBuilder, testRunResult);
+  renderTimes(resultBuilder, testModules);
 
-  renderResultSummary(resultBuilder, testRunResult);
+  renderResultSummary(resultBuilder, testModules);
 
-  const testDefinitions = resultBuilder.ele("TestDefinitions");
+  const testDefinitions = resultBuilder.ele('TestDefinitions');
 
   renderTestLists(resultBuilder);
 
-  const testEntries = resultBuilder.ele("TestEntries");
-  const results = resultBuilder.ele("Results");
+  const testEntries = resultBuilder.ele('TestEntries');
+  const results = resultBuilder.ele('Results');
 
-  testRunResult.testResults.forEach(testSuiteResult =>
-    renderTestSuiteResult(
-      testSuiteResult,
-      testDefinitions,
-      testEntries,
-      results,
-      computerName,
-      options && options.postProcessTestResult,
-    ),
-  );
+  for (const testModule of testModules) {
+    for (const testSuiteResult of testModule.children.allSuites()) {
+      renderTestSuiteResult(
+          testSuiteResult,
+          testDefinitions,
+          testEntries,
+          results,
+          computerName,
+          options && options.postProcessTestResult,
+      );
+    }
+  }
 
   return resultBuilder.end({ pretty: true });
 };
