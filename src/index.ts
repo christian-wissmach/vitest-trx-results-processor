62e7334e8b6e02bad1a5d34b349d3458623ddc0a
(External) Christian Wißmach <christian.wissmach@diamant-software.de>
2025-02-04 11:53:56 +0100

    feat(module): vitest version of the trx results processor
    If you use vitest instead of jest you can use this trx processor to create your trx reports
    
    BREAKING CHANGE: You need to change your testing framework from jest to vitest

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,31 @@
-import { AggregatedResult } from "@jest/test-result";
-import { Config } from "@jest/types";
-import { writeFileSync } from "fs";
-import mkdirp from "mkdirp";
-import path from "path";
+import { writeFileSync } from 'fs';
+import path from 'path';
+import { mkdirp } from 'mkdirp';
+import type { Reporter, TestModule } from 'vitest/node';
+import { defaultOutputFile, defaultUserName } from './constants';
+import { IOptions, generateTrx } from './trx-generator';
 
-import { defaultOutputFile, defaultUserName } from "./constants";
-import { generateTrx, IOptions } from "./trx-generator";
+class TrxReporter implements Reporter {
+  private readonly options: IOptions;
+  private testModules: TestModule[] = [];
 
-class TrxReporter {
-  private options: IOptions;
-
-  public constructor(_: Config.GlobalConfig, options: IOptions) {
+  public constructor(options: IOptions) {
     this.options = {
       ...options,
       defaultUserName: options?.defaultUserName ?? defaultUserName,
       outputFile: options?.outputFile ?? defaultOutputFile,
     };
   }
-
-  public onRunComplete = (
-    _: any,
-    aggregatedResults: AggregatedResult,
-  ): Promise<void> | void => {
-    const trx = generateTrx(aggregatedResults, this.options);
+  onTestModuleEnd(testModule: TestModule) {
+    this.testModules.push(testModule);
+    const trx = generateTrx(this.testModules, this.options);
 
     const targetDir = path.dirname(path.resolve(this.options.outputFile));
     mkdirp.sync(targetDir);
 
-    writeFileSync(this.options.outputFile, trx, { encoding: "utf8" });
-    process.stdout.write("DONE\n");
+    writeFileSync(this.options.outputFile, trx, { encoding: 'utf8' });
     process.stdout.write(`TRX file output to '${this.options.outputFile}'\n`);
-  };
+  }
 }
 
 module.exports = TrxReporter;
